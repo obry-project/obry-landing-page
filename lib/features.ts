936fa9a1945ddc8bd66a60d9ed5cd3a37c72c0d9
unknown <ronybenitez@gmail.com>
2026-03-17 14:45:47 -0300

    installing gtm and updating CTAs

---
+++ lib/features.ts
@@ -0,0 +1,4 @@
+export const FEATURES = {
+  SHOW_WAITLIST_FORM: true, // Set to true to redirect to waitlist form instead of WhatsApp
+  WAITLIST_FORM_URL: 'https://form.typeform.com/to/ZHvmItzt',
+}
